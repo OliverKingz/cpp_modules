f45c868526c5ff93078b79eaf39d1d5cc655a917
ozamora- <ozamora-@student.42madrid.com>
2025-05-26 20:11:54 +0200

    Practicing cpp

--- research/src/myfirstprogram.cpp
+++ research/src/myfirstprogram.cpp
@@ -1,11 +1,31 @@
-#include <iostream> // To use cout
-using namespace std;	// means that we can use names for objects and variables from the standard library.
-						// can be omitted and replaced with the std keyword, followed by the :: operator for some object
-						// FORBIDDEN FOR 42
+#include <iostream>	 // To use cout, cin
+#include <string> // To use string variable
+using namespace std; // means that we can use names for objects and variables from the standard library.
+					 // can be omitted and replaced with the std keyword, followed by the :: operator for some object
+					 // FORBIDDEN FOR 42
 int main()
 {
-	//cout << "Hello World";
-	std::cout << "Hello World" << std::endl;
+	// double myFloatNum = 5.99; // Floating point number (with decimals)
+	// char myLetter = 'D';	  // Character
+	// string myText = "Hello";  // String (text)
+	// bool myBoolean = true;	  // Boolean (true or false)
+
+	// Example 1
+	// int myNum = 5; // Integer (whole number without decimals)
+	// int itNum;
+	// std::cout << "Type a number like " << myNum << std::endl;
+	// cin >> itNum;
+	// std::cout << "Input number: " << itNum << std::endl;
+
+	// Example 2
+	string firstName = "Oliver";
+	string lastName = "Zamora";
+	string fullName = firstName + " " + lastName;
+	string extraName;
+	std::cout << fullName << "\n";
+	getline(cin, extraName); // Using getline for strings, as it accepts spaces without ending the string
+	std::cout << "Input extra name is: " << extraName;
+
 	return 0;
 }
 
