48c621b0034618272ce2dccb1626ce92b597570a
ozamora- <ozamora-@student.42madrid.com>
2025-05-26 19:03:57 +0200

    Make makefile for doc

---
+++ research/src/myfirstprogram.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // To use cout
+using namespace std;	// means that we can use names for objects and variables from the standard library.
+						// can be omitted and replaced with the std keyword, followed by the :: operator for some object
+						// FORBIDDEN FOR 42
+int main()
+{
+	//cout << "Hello World";
+	std::cout << "Hello World" << std::endl;
+	return 0;
+}
+
+//<<: insertion operator
