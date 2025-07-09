22ce0e8bbf2b7d7b3c5a7eb7f010b894d07f36aa
ozamora- <ozamora-@student.42madrid.com>
2025-07-09 18:05:43 +0200

    Module02 Preparation

---
+++ module02/ex00/Fixed.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 18:01:04 by ozamora-          #+#    #+#             */
+/*   Updated: 2025/07/09 18:01:51 by ozamora-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <string>
+#include <iostream>
+
+# define RESET "\033[0m"
+# define BOLD "\033[1m"
+# define BOLD_OFF "\033[22m"
+# define UNDERLINE "\033[4m"
+# define UNDERLINE_OFF "\033[24m"
+# define BLACK "\033[0;30m"
+# define WHITE "\033[0;37m"
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+# define BLUE "\033[0;34m"
+# define MAGENTA "\033[0;35m"
+# define CYAN "\033[0;36m"
+
+#endif
