16f02fa3e5affc21ce6d7300f8d753c12b25f8a5
ozamora- <ozamora-@student.42madrid.com>
2025-08-12 17:26:36 +0200

    Back to work!

--- module02/ex00/Fixed.cpp
+++ module02/ex00/Fixed.cpp
@@ -6,33 +6,38 @@
 /*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 18:01:07 by ozamora-          #+#    #+#             */
-/*   Updated: 2025/07/09 18:29:29 by ozamora-         ###   ########.fr       */
+/*   Updated: 2025/08/12 16:09:48 by ozamora-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-Fixed()
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(const Fixed& other)
 {
 	
 }
 
-Fixed(const Fixed& other)
+Fixed& Fixed::operator=(const Fixed& other)
 {
-	
+	// Add assignment logic here
+	return *this;
 }
 
-Fixed& operator=(const Fixed& other)
-
-~Fixed()
-{
-
-}
-int getRawBits(void) const
+Fixed::~Fixed()
 {
 
 }
-void setRawBits(int const raw)
+
+int Fixed::getRawBits(void) const
+{
+
+}
+void Fixed::setRawBits(int const raw)
 {
 	
 }
