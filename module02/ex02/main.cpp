31a8b6dfdb0f3393877a05ec42cd4eede215dd2a
ozamora- <ozamora-@student.42madrid.com>
2025-08-29 17:33:22 +0200

    Adding more tests. Done

--- module02/ex02/main.cpp
+++ module02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 20:54:12 by ozamora-          #+#    #+#             */
-/*   Updated: 2025/08/27 20:55:13 by ozamora-         ###   ########.fr       */
+/*   Updated: 2025/08/29 17:33:01 by ozamora-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,22 @@ int main()
 	std::cout << b << std::endl;
 
 	std::cout << Fixed::max( a, b ) << std::endl;
+
+	// More tests
+	// std::cout << "\nMy own tests: " << std::endl;
+	// Fixed const c(1);
+	// std::cout << c + c << std::endl;
+	// std::cout << c - c << std::endl;
+	// std::cout << c * c << std::endl;
+	// std::cout << c / c << std::endl;
+	// std::cout << c + a << std::endl;
+	// std::cout << a + c << std::endl;
+	// std::cout << c + b << std::endl;
+	// std::cout << b + c << std::endl;
+	// // std::cout << ++c << std::endl; // Cannot be done, as it modifies var c
+	// // std::cout << c++ << std::endl; // Cannot be done, as it modifies var c
+	// // std::cout << --c << std::endl; // Cannot be done, as it modifies var c
+	// // std::cout << c-- << std::endl; // Cannot be done, as it modifies var c
 
 	std::cout <<   "================" <<         "================================"          << "================\n\n";
 	return 0;
