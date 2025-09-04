ff04e7ec368fa1c62f2667da39a14628f30df17c
ozamora- <ozamora-@student.42madrid.com>
2025-09-04 18:29:12 +0200

    Added more tests

--- module03/ex00/main.cpp
+++ module03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 20:52:45 by ozamora-          #+#    #+#             */
-/*   Updated: 2025/09/04 18:19:25 by ozamora-         ###   ########.fr       */
+/*   Updated: 2025/09/04 18:28:37 by ozamora-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,15 @@ int main()
 	oliver.beRepaired(5);
 	oliver.takeDamage(5);
 
+	// No energy messages
+	for (size_t i = 0; i <= 8; i++) 
+		oliver.beRepaired(1);
+	oliver.attack("Brenda");
+	oliver.beRepaired(5);
+	oliver.takeDamage(5);
+
 	// Death messages
-	oliver.takeDamage(101);
+	oliver.takeDamage(200);
 	oliver.attack("Brenda");
 	oliver.beRepaired(5);
 	oliver.takeDamage(5);
