4394c09df0d9c9f15c1c9b5bd01ee54429d91d31
ozamora- <ozamora-@student.42madrid.com>
2025-09-22 17:29:11 +0200

    Implamantation all in cpp

--- module04/ex03/ICharacter.hpp
+++ module04/ex03/ICharacter.hpp
@@ -6,7 +6,7 @@
 /*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 16:17:49 by ozamora-          #+#    #+#             */
-/*   Updated: 2025/09/22 16:33:35 by ozamora-         ###   ########.fr       */
+/*   Updated: 2025/09/22 17:24:32 by ozamora-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 class ICharacter
 {
 	public:
-		virtual ~ICharacter() {}
+		virtual ~ICharacter();
 		virtual std::string const & getName() const = 0;
 		virtual void equip(AMateria* m) = 0;
 		virtual void unequip(int idx) = 0;
--- module04/ex03/IMateriaSource.cpp
+++ module04/ex03/IMateriaSource.cpp
@@ -1,24 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   IMateriaSource.hpp                                 :+:      :+:    :+:   */
+/*   IMateriaSource.cpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/22 16:17:22 by ozamora-          #+#    #+#             */
-/*   Updated: 2025/09/22 17:26:02 by ozamora-         ###   ########.fr       */
+/*   Created: 2025/09/22 17:24:55 by ozamora-          #+#    #+#             */
+/*   Updated: 2025/09/22 17:28:55 by ozamora-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
+#include "IMateriaSource.hpp"
 
-#include "colors.hpp"
-#include "AMateria.hpp"
-
-class IMateriaSource
-{
-	public:
-		virtual ~IMateriaSource();
-		virtual void learnMateria(AMateria*) = 0;
-		virtual AMateria* createMateria(std::string const & type) = 0;
-};
+IMateriaSource::~IMateriaSource() {}
