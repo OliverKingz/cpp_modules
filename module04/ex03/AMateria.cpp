d578d1166b54aa9e50cf66f5318d1edbf575e959
ozamora- <ozamora-@student.42madrid.com>
2025-09-22 16:59:52 +0200

    finished AMateria

--- module04/ex03/AMateria.cpp
+++ module04/ex03/AMateria.cpp
@@ -6,8 +6,35 @@
 /*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 16:25:56 by ozamora-          #+#    #+#             */
-/*   Updated: 2025/09/22 16:28:36 by ozamora-         ###   ########.fr       */
+/*   Updated: 2025/09/22 16:56:52 by ozamora-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AMateria.hpp"
+
+AMateria::AMateria(std::string const & type) : _type(type){
+	std::cout << "AMateria Constructor called\n";
+}
+
+AMateria::AMateria() : _type("") {
+	std::cout << "AMateria Default Constructor called\n";
+}
+
+AMateria::AMateria(const AMateria& src) : _type(src._type){
+	std::cout << "AMateria Copy Constructor called\n";
+}
+
+AMateria::~AMateria() {
+	std::cout << "AMateria Destructor called\n";
+}
+
+AMateria& AMateria::operator=(const AMateria& src) {
+	std::cout << "AMateria Copy Assignment Operator called\n";
+	if (this != &src)
+		this->_type = src._type;
+	return *this;
+}
+
+std::string const & AMateria::getType() const { return _type; }
+
+void AMateria::use(ICharacter& target) { (void)target; }
--- module04/ex03/AMateria.hpp
+++ module04/ex03/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 16:17:11 by ozamora-          #+#    #+#             */
-/*   Updated: 2025/09/22 16:33:09 by ozamora-         ###   ########.fr       */
+/*   Updated: 2025/09/22 16:45:00 by ozamora-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,20 @@
 
 #include "colors.hpp"
 #include "ICharacter.hpp"
+#include "IMateriaSource.hpp"
 
 class AMateria
 {
 	protected:
+		std::string _type;
 	public:
 		AMateria(std::string const & type);
+
+		AMateria();
+		AMateria(const AMateria& src);
+		~AMateria();
+		AMateria& operator=(const AMateria& src);
+
 		std::string const & getType() const; //Returns the materia type
 		virtual AMateria* clone() const = 0;
 		virtual void use(ICharacter& target);
