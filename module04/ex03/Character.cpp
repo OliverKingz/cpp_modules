39b9561451d32eae85e6652f4ddab78d0b57555f
ozamora- <ozamora-@student.42madrid.com>
2025-09-23 13:03:12 +0200

    Finished Orthodox

--- module04/ex03/Character.cpp
+++ module04/ex03/Character.cpp
@@ -6,8 +6,85 @@
 /*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 16:26:04 by ozamora-          #+#    #+#             */
-/*   Updated: 2025/09/22 16:29:01 by ozamora-         ###   ########.fr       */
+/*   Updated: 2025/09/23 13:02:05 by ozamora-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Character.hpp"
+#include "AMateria.hpp"
+
+Character::Character(void) : _name(""), _n_materias(0), _dropped(NULL), _n_drop(0) {
+	std::cout << "Character Default Constructor called for: " BLUE << _name << RESET << std::endl;
+	for (int i = 0 ; i < 4; i++)
+		_inventory[i] = NULL;
+}
+
+Character::Character(void) : _name(""), _n_materias(0), _n_drop(0) {
+	std::cout << "Character Default Constructor called for: " BLUE << _name << RESET << std::endl;;
+}
+
+Character::Character(std::string name) : _name(name), _n_materias(0), _dropped(NULL), _n_drop(0){
+	std::cout << "Character Constructor called for: " BLUE << _name << RESET << std::endl;;
+	for (int i = 0 ; i < 4; i++)
+		_inventory[i] = NULL;
+}
+
+Character::Character(const Character& src) : _name(src._name), _n_materias(src._n_materias), _dropped(src._dropped), _n_drop(src._n_drop){
+	std::cout << "Character Copy Constructor called for: " BLUE << _name << RESET << std::endl;;
+	for (int i = 0 ; i < 4; i++)
+	{
+		if (i < src._n_materias)
+			_inventory[i] = src._inventory[i]->clone(); // Deep object copy
+		else
+			_inventory[i] = NULL;
+	}
+}
+
+Character::~Character() {
+	std::cout << "Character Destructor called for: " BLUE << _name << RESET << std::endl;;
+	for (int i = 0 ; i < 4; i++)
+	{
+		if (i < _n_materias)
+		{
+			delete _inventory[i];
+			_inventory[i] = NULL;
+		}
+		else
+			_inventory[i] = NULL;
+	}
+}
+
+Character& Character::operator=(const Character& src) {
+	std::cout << "Character Copy Assignment Operator called for: " BLUE << _name << RESET << std::endl;;
+	if (this != &src)
+	{
+		this->_name = src._name;
+		for (int i = 0 ; i < 4; i++)
+		{
+			if (i < src._n_materias)
+				this->_inventory[i] = src._inventory[i]->clone(); // Deep object copy
+			else
+				this->_inventory[i] = NULL;
+		}
+		this->_n_materias = src._n_materias;
+		this->_dropped = src._dropped;
+		this->_n_drop = src._n_drop;
+	}
+	return *this;
+}
+
+std::string const & Character::getName() const {
+	return _name;
+}
+
+void Character::equip(AMateria* m) {
+	
+}
+
+void Character::unequip(int idx) {
+	
+}
+
+void Character::use(int idx, ICharacter& target) {
+	
+}
--- module04/ex03/Character.hpp
+++ module04/ex03/Character.hpp
@@ -6,15 +6,34 @@
 /*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 16:25:43 by ozamora-          #+#    #+#             */
-/*   Updated: 2025/09/22 16:30:10 by ozamora-         ###   ########.fr       */
+/*   Updated: 2025/09/23 12:47:50 by ozamora-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include "ICharacter.hpp"
+#include "ICharacter.hpp" // It needs to still include AMateria in cpp
 
 class Character : public ICharacter
 {
+	private:
+		std::string	_name;
+		AMateria*	_inventory[4];
+		size_t		_n_materias;
+		AMateria**	_dropped;
+		size_t		_n_drop;
+
+	public:
+		Character(void);
+		Character(std::string name);
+		Character(const Character& src);
+		~Character(void);
+
+		Character& operator=(const Character& src);
+
+		std::string const & getName() const;
+		void equip(AMateria* m);
+		void unequip(int idx);
+		void use(int idx, ICharacter& target);
 
 };
