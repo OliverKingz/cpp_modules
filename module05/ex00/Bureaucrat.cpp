728ac00a1a5fc70bb51e3ad8e5c4f669c87ce2c0
OliverKingz <oliverkingz@gmail.com>
2025-12-11 17:56:35 +0100

    Starting cpp

--- module05/ex00/Bureaucrat.cpp
+++ module05/ex00/Bureaucrat.cpp
@@ -1,17 +1,21 @@
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat(void) {
+Bureaucrat::Bureaucrat(void) : _name("Noname"), _grade(150){
 	std::cout << "Bureaucrat Default Constructor called\n";
 }
 
+Bureaucrat::Bureaucrat(const std::string& name, const int grade) : _name(name), _grade(grade){
+	std::cout << "Bureaucrat Constructor called for " << _name << ".\n";
+}
+
 Bureaucrat::Bureaucrat(const Bureaucrat& src) {
-	std::cout << "Bureaucrat Copy Constructor called\n";
+	std::cout << "Bureaucrat Copy Constructor called for " << _name << ".\n";
 }
 
 Bureaucrat::~Bureaucrat() {
-	std::cout << "Bureaucrat Destructor called\n";
+	std::cout << "Bureaucrat Destructor called for " << _name << ".\n";
 }
 
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat& src) {
-	std::cout << "Bureaucrat Copy Assignment Operator called\n";
+	std::cout << "Bureaucrat Copy Assignment Operator called for " << _name << ".\n";
 }
