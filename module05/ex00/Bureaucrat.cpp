5d696d83f77a218f110997f2b578d747fcc72881
ozamora- <ozamora-@student.42madrid.com>
2025-12-18 22:11:42 +0100

    WIP cpp

--- module05/ex00/Bureaucrat.cpp
+++ module05/ex00/Bureaucrat.cpp
@@ -1,21 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 21:33:25 by ozamora-          #+#    #+#             */
+/*   Updated: 2025/12/18 22:11:01 by ozamora-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Bureaucrat.hpp"
 
+// ======| Constructors and Destructors (Canonical) |======
 Bureaucrat::Bureaucrat(void) : _name("Noname"), _grade(150){
 	std::cout << "Bureaucrat Default Constructor called\n";
 }
 
-Bureaucrat::Bureaucrat(const std::string& name, const int grade) : _name(name), _grade(grade){
+Bureaucrat::Bureaucrat(const std::string& name, const int grade)
+	: _name(name), _grade(grade) {
+	if (grade < 1)
+		throw GradeTooHighException(_name);
+	else if (grade > 150)
+		throw GradeTooLowException(_name);
 	std::cout << "Bureaucrat Constructor called for " << _name << ".\n";
 }
 
-Bureaucrat::Bureaucrat(const Bureaucrat& src) {
+Bureaucrat::Bureaucrat(const Bureaucrat& src) 
+	: _name(src._name), _grade(src._grade) {
 	std::cout << "Bureaucrat Copy Constructor called for " << _name << ".\n";
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& src) {
+	if (this != &src)
+	{
+		_grade = src._grade;
+		//_name = src._name; // Cannot be changed, as it is const. LOOK LIST
+	}
+	std::cout << "Bureaucrat Copy Assignment Operator called for " << _name << ".\n";
 }
 
 Bureaucrat::~Bureaucrat() {
 	std::cout << "Bureaucrat Destructor called for " << _name << ".\n";
 }
 
-Bureaucrat& Bureaucrat::operator=(const Bureaucrat& src) {
-	std::cout << "Bureaucrat Copy Assignment Operator called for " << _name << ".\n";
+// ======| Getters |======
+const std::string Bureaucrat::getName() const {
+
 }
+
+int Bureaucrat::getGrade() const{
+
+}
+
+// ======| Setters |======
+void Bureaucrat::incrementGrade(void) {
+
+}
+
+void Bureaucrat::decrementGrade(void){
+
+}
+
+// ======| Internal Exception GradeTooHighException (Subject: non-canonical) |======
+Bureaucrat::GradeTooHighException::GradeTooHighException(const std::string& name){
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw(){
+}
+
+// ======| Internal Exception GradeTooLowException (Subject: non-canonical) |======
+Bureaucrat::GradeTooLowException::GradeTooLowException(const std::string& name){
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw(){
+}
+
+// ======| Operator << |======
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& person);
--- module05/ex00/Bureaucrat.hpp
+++ module05/ex00/Bureaucrat.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 21:33:19 by ozamora-          #+#    #+#             */
+/*   Updated: 2025/12/18 22:05:09 by ozamora-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
 
 #include <string>
@@ -11,36 +23,40 @@ class Bureaucrat
 		const std::string	_name;
 		int					_grade; // 0 to 150
 	public:
-		// Constructors and Destructors
+		// ===| Constructors and Destructors (Canonical) |===
 		Bureaucrat(void);
 		Bureaucrat(const std::string& name, const int grade); //Throws exception Bureaucrat::GradeTooHighException or a Bureaucrat::GradeTooLowException
 		Bureaucrat(const Bureaucrat& src);
 		Bureaucrat& operator=(const Bureaucrat& src);
 		~Bureaucrat(void);
 
-		// Getters
+		// ===| Getters
 		const std::string getName() const;
 		int getGrade() const; 
 
-		// Setters
+		// ===| Setters
 		void incrementGrade(void); //Throws Internal exception
 		void decrementGrade(void); //Throws Internal exception
 
-		// Internal Exceptions (Subject: non-canonical)
-		class GradeTooHighException{
+		// ===| Internal Exception GradeTooHighException (Subject: non-canonical) |===
+		class GradeTooHighException : public std::exception{
 			private:
 				const std::string _msg;
 			public:
-				GradeTooHighException(const std::string& name);
-				const std::string& what() const;
+				GradeTooHighException(const std::string& name); // name is the object that gave the error
+				virtual ~GradeTooHighException() throw(); // throw() is the old C++98 dynamic exception specification meaning "this function throws nothing", noexcept is a C++11 feature
+				virtual const char *what() const throw(); // Cannot be string, override from exception
 		};
-		class GradeTooLowException{
+		// ===| Internal Exception GradeTooLowException (Subject: non-canonical) |===
+		class GradeTooLowException : public std::exception{
 			private:
 				const std::string _msg;
 			public:
 				GradeTooLowException(const std::string& name);
-				const std::string	&what() const;
+				virtual ~GradeTooLowException() throw(); // virtual kept for clarity, but it's not required
+				virtual const char *what() const throw();
 		};
 };
 
+// ===| Operator << |===
 std::ostream& operator<<(std::ostream& os, const Bureaucrat& person);
