c3d001dcc1857884a5843f78761b9bfe76cf7996
ozamora- <ozamora-@student.42madrid.com>
2026-07-28 19:44:28 +0200

    finished m6ex02

--- module06/ex02/Base.cpp
+++ module06/ex02/Base.cpp
@@ -1,5 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/28 18:54:22 by ozamora-          #+#    #+#             */
+/*   Updated: 2026/07/28 19:42:51 by ozamora-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Base.hpp"
 #include "colors.hpp"
+#include <cstdlib> // To use rand
 
 /*
  * Debug macro: activate by defining DEBUG during compilation
@@ -11,23 +24,86 @@
  */
 
 #ifdef DEBUG
- # define DBG_MSG(x) std::cout << "Class " << x << std::endl
+ # define DBG_MSG(x) std::cout << x << std::endl
 #else
  # define DBG_MSG(x) ((void)0)
 #endif
 
+/* ************************************************************************** */
+
 Base::Base(void) {
-	DBG_MSG("Default Constructor called");
-}
-
-Base::Base(const Base& src) {
-	DBG_MSG("Copy Constructor called");
-}
-
-Base& Base::operator=(const Base& src) {
-	DBG_MSG(<< "Copy Assignment Operator called");
+	DBG_MSG("Base Default Constructor called");
 }
 
 Base::~Base(void) {
-	DBG_MSG("Destructor called");
+	DBG_MSG("Base Destructor called");
 }
+
+/* ************************************************************************** */
+
+A::A(void) {
+	std::cout << "Default Constructor called for A" << std::endl;
+}
+
+/* ************************************************************************** */
+
+B::B(void) {
+	std::cout << "Default Constructor called for B" << std::endl;
+}
+
+/* ************************************************************************** */
+
+C::C(void) {
+	std::cout << "Default Constructor called for C" << std::endl;
+}
+
+/* ************************************************************************** */
+
+Base * generate(void)
+{
+	int randNum = std::rand() % 3;
+
+	if ( randNum == 0 )
+		return new A();
+	else if ( randNum == 1 )
+		return new B();
+	else
+		return new C(); 
+}
+
+void identify(Base* p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "The object is A (ptr)" << std::endl;
+	else if(dynamic_cast<B*>(p))
+		std::cout << "The object is B (ptr)" << std::endl;
+	else if(dynamic_cast<C*>(p))
+		std::cout << "The object is C (ptr)" << std::endl;
+	else
+		std::cout << "The object is Unknown (ptr)" << std::endl;
+}
+
+void identify(Base& p)
+{
+	try {
+		(void)(dynamic_cast<A&>(p)); // (void) to avoid the warning unused-value
+		std::cout << "The object is A (ref)" << std::endl;
+		return ;
+	} catch (std::exception&){}
+
+	try {
+		(void)(dynamic_cast<B&>(p));
+		std::cout << "The object is B (ref)" << std::endl;
+		return ;
+	} catch (std::exception&){}
+
+	try {
+		(void)(dynamic_cast<C&>(p));
+		std::cout << "The object is C (ref)" << std::endl;
+		return ;
+	} catch (std::exception&){}
+
+	std::cout << "The object is Unknown (ref)" << std::endl;
+}
+
+/* ************************************************************************** */
--- module06/ex02/main.cpp
+++ module06/ex02/main.cpp
@@ -1,11 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/28 19:03:14 by ozamora-          #+#    #+#             */
+/*   Updated: 2026/07/28 19:40:05 by ozamora-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "colors.hpp"
-#include "Class.hpp"
+#include "Base.hpp"
+#include <cstdlib> // To use rand
+#include <ctime> // To use srand (seed)
 
 int main()
 {
-	std::cout << "\n===============|" << BLUE << " Exercise X: template of main " << RESET << "|===============\n";
-	Base *example = new Base();
-	delete example;	
+	std::cout << "\n===============|" << BLUE << " Exercise 02: Identify real type " << RESET << "|===============\n";
+
+	std::srand(std::time(NULL)); // Seed that ensures true randomness
+
+	std::cout << BLUE "\n - Identifying type using pointers" RESET << std::endl;
+	{
+		for (int i = 0; i < 5; i++) {
+			Base *ptr = generate();
+			identify(ptr);
+			delete ptr;
+		}
+	}
+
+	std::cout << BLUE "\n - Identifying type using references" RESET << std::endl;
+	{
+		for (int i = 0; i < 5; i++) {
+			Base *ptr = generate();
+			identify(*ptr);
+			delete ptr;
+		}
+	}
+
+	std::cout << BLUE "\n - Identifying type using both ptr and ref" RESET << std::endl;
+	{
+		for (int i = 0; i < 5; i++) {
+			Base *ptr = generate();
+			identify(*ptr);
+			identify(ptr);
+			delete ptr;
+		}
+	}
+
+	std::cout << BLUE "\n - Edge case: unknown" RESET << std::endl;
+	{
+		Base *ptr = NULL;
+		identify(*ptr);
+		identify(ptr);
+		if (ptr) delete ptr;
+
+		Base base = Base();
+		identify(base);
+		identify(&base);
+	}
+	
 	std::cout <<   "================" <<         "=============================="          << "================\n\n";
 	return 0;
 }
