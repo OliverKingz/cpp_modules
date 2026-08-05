5078f12a7779c519f832247407b7f776f771410d
OliverKingz <oliverkingz@gmail.com>
2026-08-05 20:15:54 +0200

    Finished m7ex00

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -6,21 +6,56 @@
 /*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 18:59:42 by ozamora-          #+#    #+#             */
-/*   Updated: 2026/08/05 19:00:11 by ozamora-         ###   ########.fr       */
+/*   Updated: 2026/08/05 20:12:51 by ozamora-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
 
-#include <string>
-#include <iostream>
-#include "colors.hpp"
+#include <algorithm> // To use std::find
+#include <exception> // To use std::exception
 
-class easyfind
+class NotFoundException : public std::exception
 {
 	public:
-		easyfind(void);
-		easyfind(const easyfind& src);
-		easyfind& operator=(const easyfind& src);
-		~easyfind(void);
+		const char* what() const throw() { return "Element not found in container";}
 };
+
+// std::find(begin, end, value) searches in [begin, end) and returns an iterator:
+// - if found: iterator to the first matching element
+// - if not found: end() iterator
+
+// In a template, T::iterator depends on T, so we must write "typename T::iterator"
+// to tell the compiler that T::iterator is a type.
+
+// Example: 
+// T == std::vector<int>;
+// typename T::iterator == std::vector<int>::iterator
+
+// Example with const:
+// T == const std::list<int>
+// typename T::const_iterator == std::list<int>::const_iterator
+
+
+template<typename T>
+typename T::iterator		easyfind(T& container, int n_toBeFound)
+{
+	typename T::iterator it = std::find(container.begin(), container.end(), n_toBeFound);
+	if (it == container.end())
+		throw NotFoundException();
+	return it;
+}
+
+template<typename T>
+typename T::const_iterator	easyfind(const T& container, int n_toBeFound)
+{
+	typename T::const_iterator it = std::find(container.begin(), container.end(), n_toBeFound);
+	if (it == container.end())
+		throw NotFoundException();
+	return it;
+}
+
+// Good practice to add the const version override as well
+
+#endif
--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -6,16 +6,71 @@
 /*   By: ozamora- <ozamora-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 19:08:01 by ozamora-          #+#    #+#             */
-/*   Updated: 2026/08/05 19:08:14 by ozamora-         ###   ########.fr       */
+/*   Updated: 2026/08/05 20:15:30 by ozamora-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "colors.hpp"
 #include "easyfind.hpp"
 
+#include <iostream> // To use std::cout
+#include <vector>   // To use std::vector
+#include <list>     // To use std::list
+#include <deque>    // To use std::deque
+
+// std::map does not work here because easyfind uses std::find,
+// which compares the container's element type directly with an int.
+
 int main()
 {
 	std::cout << "\n===============|" << BLUE << " Exercise 00: Easy find " << RESET << "|===============\n";
+	std::cout << GREEN "\nTesting std::vector \n" RESET;
+	
+	{
+		std::vector<int> vec;
+		for (int i = 0; i < 10; ++i)
+			vec.push_back(i);
+
+		try {
+			int valueToFind = 5;
+			std::vector<int>::iterator it = easyfind(vec, valueToFind); // remember to use the correct iterator type
+			std::cout << "Found " << valueToFind << " in vector at index: " << *it << std::endl;
+		} catch (const NotFoundException& e) {
+			std::cerr << e.what() << std::endl;
+		}
+	}
+
+	std::cout << GREEN "\nTesting std::list \n" RESET;
+	{
+		std::list<int> lst;
+		for (int i = 0; i < 10; ++i)
+			lst.push_back(i);
+
+		try {
+			int valueToFind = 20;
+			std::list<int>::iterator it = easyfind(lst, valueToFind);
+			// Another way to get the index is to use std::distance
+			std::cout << "Found " << valueToFind << " in list at index: " << std::distance(lst.begin(), it) << std::endl;
+		} catch (const NotFoundException& e) {
+			std::cerr << e.what() << std::endl;
+		}
+	}
+
+	std::cout << GREEN "\nTesting std::deque \n" RESET;
+	{
+		std::deque<int> dq;
+		for (int i = 0; i < 10; ++i)
+			dq.push_back(i);
+
+		try {
+			int valueToFind = 7;
+			std::deque<int>::iterator it = easyfind(dq, valueToFind);
+			std::cout << "Found " << valueToFind << " in deque at index: " << *it << std::endl;
+		} catch (const NotFoundException& e) {
+			std::cerr << e.what() << std::endl;
+		}
+	}
+
 	std::cout <<   "================" <<         "========================"          << "================\n\n";
 	return 0;
 }
